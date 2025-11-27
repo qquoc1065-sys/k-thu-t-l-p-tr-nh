0bf3e5e55f0f781d3fa8721db809ce0eacea5db0
trongtinh38 <trongtinh0910@gmail.com>
2025-11-27 22:28:26 +0700

    Add files via upload

---
+++ baitap4 c2.py
@@ -0,0 +1,7 @@
+width = 17
+height = 12.0
+
+print("1. width//2 =", width // 2, ", type:", type(width // 2))
+print("2. width/2.0 =", width / 2.0, ", type:", type(width / 2.0))
+print("3. height/3 =", height / 3, ", type:", type(height / 3))
+print("4. 1 + 2 * 5 =", 1 + 2 * 5, ", type:", type(1 + 2 * 5))
